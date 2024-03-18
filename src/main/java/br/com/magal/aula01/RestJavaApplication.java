f09bf78d89b98925ead6ed48ee55c210ee98aec6
jcmagal <joaoc.magalhaes97@gmail.com>
2024-03-18 10:19:42 -0400

    aula01

---
+++ src/main/java/br/com/magal/aula01/RestJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.magal.aula01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestJavaApplication.class, args);
+	}
+
+}
